71338b8504f483274c6ab111fe94e298abdb3c60
Antony <>
2022-09-14 19:15:37 +0700

    remake inp name in constructors, unary minus

--- main.cpp
+++ main.cpp
@@ -23,11 +23,11 @@ public:
 //
 //    ~BigInt();
 //
-//    BigInt &operator=(const BigInt &);  //возможно присваивание самому себе!
+    BigInt &operator=(const BigInt &);  //возможно присваивание самому себе!
 //
 //    BigInt operator~() const;
 //
-    //  BigInt &operator++();
+//    BigInt &operator++();
 //
 //    const BigInt operator++(int) const;
 //
@@ -35,7 +35,7 @@ public:
 //
 //    const BigInt operator--(int) const;
 //
-//    BigInt &operator+=(const BigInt &);
+    BigInt &operator+=(const BigInt &);
 //
 //    BigInt &operator*=(const BigInt &);
 //
@@ -51,8 +51,8 @@ public:
 //
 //    BigInt &operator|=(const BigInt &);
 //
-//    BigInt operator+() const;  // unary +
-//    BigInt operator-() const;  // unary -
+      BigInt operator+() const;  // unary +
+      BigInt & operator-();  // unary -
 //
 //    bool operator==(const BigInt &) const;
 //
@@ -73,68 +73,6 @@ public:
 //    size_t size() const;  // size in bytes
 };
 
-BigInt::BigInt() {
-    number.push_back(0);
-    sign = '+';
-}
-
-BigInt::BigInt(long num1) {
-    if (num1 < 0)
-        sign = '-';
-    num1 = abs(num1);
-    while (num1 > 0) {
-        number.push_back(num1 % syst);
-        num1 = num1 / syst;
-    }
-    std::reverse(number.begin(), number.end());
-}
-
-BigInt::BigInt(std::string num2) {
-    if (num2.empty() || (num2[0] == '-' && num2.length() < 2))
-        throw std::invalid_argument("string is too short");
-
-    if (num2[0] != '-' && (num2[0] < '0' || num2[0] > '9'))
-        throw std::invalid_argument("first symbol is invalid");
-
-    for (int i = 1; i < num2.length(); i++)
-        if (num2[i] < '0' || num2[i] > '9')
-            throw std::invalid_argument("string contains invalid symbol");
-
-    if (num2[0] == '-') {
-        sign = '-';
-        num2.erase(0, 1);
-    }
-    while (num2.length() >= 9) {
-        number.push_back(std::stoi(num2.substr(num2.length() - 9, 9)));
-        num2.erase(num2.length() - 9, 9);
-    }
-    if (num2.length() != 0)
-        number.push_back(std::stoi(num2));
-
-    std::reverse(number.begin(), number.end());
-}
-
-BigInt::BigInt(const BigInt &input_bi) {
-    number = input_bi.number;
-    sign = input_bi.sign;
-}
-
-//BigInt &BigInt::operator++() {
-
-//}
-
-
-
-
-void PRINT_BI(const BigInt &bi) {
-    bi.sign == '-' ? std::cout << bi.sign : std::cout << "";
-    std::cout << bi.number[0];
-    for (int i = 1; i < bi.number.size(); i++) {
-        printf("%09d", bi.number[i]);
-    }
-}
-
-
 //
 //BigInt operator+(const BigInt&, const BigInt&);
 //BigInt operator-(const BigInt&, const BigInt&);
@@ -147,9 +85,97 @@ void PRINT_BI(const BigInt &bi) {
 //
 //
 //std::ostream& operator<<(std::ostream& o, const BigInt& i);
+
+BigInt::BigInt() {
+    number.push_back(0);
+}
+
+BigInt::BigInt(long inp_int) {
+    if (inp_int < 0)
+        sign = '-';
+    inp_int = abs(inp_int);
+    while (inp_int > 0) {
+        number.push_back(inp_int % syst);
+        inp_int = inp_int / syst;
+    }
+
+    std::reverse(number.begin(), number.end());
+
+    while (number[0] == 0)
+        number.erase(number.begin());
+}
+
+BigInt::BigInt(std::string inp_str) {
+    if (inp_str.empty() || (inp_str[0] == '-' && inp_str.length() < 2))
+        throw std::invalid_argument("string is too short");
+
+    if (inp_str[0] != '-' && (inp_str[0] < '0' || inp_str[0] > '9'))
+        throw std::invalid_argument("first symbol is invalid");
+
+    for (int i = 1; i < inp_str.length(); i++)
+        if (inp_str[i] < '0' || inp_str[i] > '9')
+            throw std::invalid_argument("string contains invalid symbol");
+
+    if (inp_str[0] == '-') {
+        sign = '-';
+        inp_str.erase(0, 1);
+    }
+    while (inp_str.length() >= 9) {
+        number.push_back(std::stoi(inp_str.substr(inp_str.length() - 9, 9)));
+        inp_str.erase(inp_str.length() - 9, 9);
+    }
+    if (inp_str.length() != 0)
+        number.push_back(std::stoi(inp_str));
+
+    std::reverse(number.begin(), number.end());
+
+    while (number[0] == 0 && number.size() > 1)
+        number.erase(number.begin());
+}
+
+BigInt::BigInt(const BigInt &inp_bi) {
+    number = inp_bi.number;
+    sign = inp_bi.sign;
+}
+
+BigInt &BigInt::operator=(const BigInt &inp_bi) {
+    number = inp_bi.number;
+    sign = inp_bi.sign;
+    return *this;
+}
+
+//BigInt &BigInt::operator+=(const BigInt &inp_bi) {
+//    int carry = 0;
+//    int max_len = std::max(number.size(),inp_bi.number.size());
+//    for (int i = )
+//
+//}
+
+BigInt & BigInt::operator-() {
+    if (sign =='+')
+        sign = '-';
+    else
+        sign = '+';
+    return *this;
+}
+
+BigInt BigInt::operator+() const {
+    return *this;
+}
+
+
+void PRINT_BI(const BigInt &inp_bi) {
+    inp_bi.sign == '-' ? std::cout << inp_bi.sign : std::cout << "";
+    std::cout << inp_bi.number[0];
+    for (int i = 1; i < inp_bi.number.size(); i++) {
+        printf("%09d", inp_bi.number[i]);
+    }
+}
+
+
 int main() {
     try {
-        BigInt bi("0000000000112300000000");
+        BigInt bi("0000000000000000000000000000001001");
 
         PRINT_BI(bi);
     }
